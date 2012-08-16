877e4e0bf4a14b9bd3610e6fadaa8e6b02ef2a1b
Aidan Ryan <ryan.aidan@gmail.com>
2012-08-16 01:01:27 -0700

    Attempt alternate TFS connection method

--- TfsManualTester.Web/Controllers/AccessController.cs
+++ TfsManualTester.Web/Controllers/AccessController.cs
@@ -3,6 +3,7 @@ using System.Net;
 using System.Web.Mvc;
 using Microsoft.TeamFoundation;
 using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.TestManagement.Client;
 
 namespace TfsManualTester.Web.Controllers
 {
@@ -14,18 +15,24 @@ namespace TfsManualTester.Web.Controllers
         }
 
         [HttpPost]
-        public JsonResult TfsLogin(string tfsUrl, string userName, string password)
+        public JsonResult TfsLogin(string tfsUrl, string userName, string password, string domain)
         {
             bool success = true;
             string errorMessage = null;
+            int planCount = 0;
 
             try
             {
                 var tfs = new TfsTeamProjectCollection(
                     new Uri(tfsUrl),
-                    new NetworkCredential(userName, password));
+                    new NetworkCredential(userName, password, domain));
                 
                 tfs.EnsureAuthenticated();
+
+                var testManagement = tfs.GetService<ITestManagementService>();
+                var testProject = testManagement.GetTeamProject("RealTimeMessaging");
+                var testPlans = testProject.TestPlans.Query("SELECT * FROM TestPlan");
+                planCount = testPlans.Count;
             }
             catch (Exception authEx)
             {
@@ -40,7 +47,9 @@ namespace TfsManualTester.Web.Controllers
                     ErrorMessage = errorMessage,
                     TfsUrl = tfsUrl,
                     UserName = userName,
-                    Password = password
+                    Password = password,
+                    Domain = domain,
+                    PlanCount = planCount
                 });
         }
     }
