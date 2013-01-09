55310de558b68954917a10d0822e8bdf88cb8354
Aidan Ryan <ryan.aidan@gmail.com>
2013-01-09 09:43:08 -0800

    Stub WorkItems API endpoint

---
+++ TfsManualTester.Web/Controllers/WorkItemsController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace TfsManualTester.Web.Controllers
+{
+    public class WorkItemsController : ApiController
+    {
+        // GET api/workitems
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
--- TfsManualTester.Web/Global.asax.cs
+++ TfsManualTester.Web/Global.asax.cs
@@ -1,16 +1,11 @@
-﻿using System.Configuration;
-using System.Reflection;
-using System.Web.Http;
+﻿using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using Raven.Client.Document;
-using Raven.Client.Indexes;
 
 namespace TfsManualTester.Web
 {
-    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-    // visit http://go.microsoft.com/?LinkId=9394801
     public class MvcApplication : System.Web.HttpApplication
     {
         public static DocumentStore Store;
@@ -24,7 +19,6 @@ namespace TfsManualTester.Web
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
 
-            //string ravenHqConnectionString = ConfigurationManager.AppSettings["RAVENHQ_CONNECTION_STRING"];
             Store = new DocumentStore {ConnectionStringName = "RavenDB"};
             Store.Initialize();
 
