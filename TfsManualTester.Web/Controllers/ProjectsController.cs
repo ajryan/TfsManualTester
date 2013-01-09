bf951782c081b70767a5ac5a51f8a5d294085e88
Aidan Ryan <ryan.aidan@gmail.com>
2013-01-09 09:52:36 -0800

    Stub Projects API endpoint

---
+++ TfsManualTester.Web/Controllers/ProjectsController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace TfsManualTester.Web.Controllers
+{
+    public class ProjectsController : ApiController
+    {
+        // GET api/projects
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
